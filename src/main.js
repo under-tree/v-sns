b5f2ff10813b6eafe37ba1f15cc85e1a018d0f92
Logan <1009728013@qq.com>
2024-07-29 16:22:50 +0800

    fix: init pinia

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import 'virtual:uno.css'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 app.mount('#app')
