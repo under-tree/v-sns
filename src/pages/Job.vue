ad5bca8e7bdec66a4b3dce474ff1d3be0f7c1bb1
Logan <1009728013@qq.com>
2024-07-29 20:23:25 +0800

    feat: job

---
+++ src/pages/Job.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ItemList from '../components/ItemList.vue'
+</script>
+
+<template>
+  <div class="max-w-150 m-0 m-auto">
+    <ItemList />
+  </div>
+</template>
+
+<style scoped></style>
