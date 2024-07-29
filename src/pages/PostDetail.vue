7115ae8396d26ce7379cd0f69c828312cdf117a5
Logan <1009728013@qq.com>
2024-07-29 20:43:19 +0800

    feat: post-detail

---
+++ src/pages/PostDetail.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { marked } from 'marked'
+import { getPost } from '../apis/api'
+
+onMounted(() => {
+  getPost(route.params.id).then(res => {
+    data.value = res.data
+  })
+})
+
+const route = useRoute()
+const data = ref({})
+
+</script>
+
+<template>
+  <main class="max-w-180 m-0 m-auto mb-8">
+
+    <div class="markdown-body mb-6 p-6 rounded-xl shadow-2xl bg-white" v-html="marked(data.content || '')"></div>
+
+    <div class="p-6 rounded-xl shadow-2xl bg-white">
+      <a-comment v-for="comment in data.comments">
+
+        <template #actions>
+          <span key="comment-basic-reply-to">Reply to</span>
+        </template>
+
+        <template #author><a>{{ comment.nickname }}</a></template>
+
+        <template #avatar>
+          <a-avatar :src="comment.avatar" />
+        </template>
+
+        <template #content>
+          <p>{{ comment.content }}</p>
+        </template>
+
+      </a-comment>
+    </div>
+
+  </main>
+</template>
+
+<style scoped></style>
