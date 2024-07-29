683cbc9d369a8f2846f3cdfcbbe0ce3d155bb5e7
Logan <1009728013@qq.com>
2024-07-29 21:03:58 +0800

    feat: update router.js

--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
-import About from './pages/About.vue'
-import Editor from './components/Editor.vue'
+import Activity from './pages/Activity.vue'
+import Job from './pages/Job.vue'
+import User from './pages/User.vue'
+import PostDetail from './pages/PostDetail.vue'
+import Editor from './pages/Editor.vue'
 
 const routes = [
   { 'path': '/', 'component': Home },
-  { 'path': '/about', 'component': About },
+  { 'path': '/activity', 'component': Activity },
+  { 'path': '/job', 'component': Job },
+  { 'path': '/user', 'component': User },
+  { 'path': '/users/:id', 'component': User },
+  { 'path': '/posts/:id', 'component': PostDetail },
   { 'path': '/editor', 'component': Editor },
 ]
 
