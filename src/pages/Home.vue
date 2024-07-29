5d5c7fb3f8ca2710da3efbe3763c661835fa3f72
Logan <1009728013@qq.com>
2024-07-29 20:22:33 +0800

    feat: home demo

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import ItemList from '../components/ItemList.vue'
+import Sidebar from '../components/Sidebar.vue'
 </script>
 
 <template>
-  <h2>Home</h2>
+  <div class="max-w-250 m-0 m-auto flex">
+
+    <main class="w-3/5 mx-8">
+      <ItemList />
+    </main>
+
+    <div>
+      <Sidebar />
+    </div>
+
+  </div>
 </template>
 
 <style scoped></style>
