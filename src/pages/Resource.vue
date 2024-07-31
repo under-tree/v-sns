abdbbbac8baa40512816f761f1d7c21c0da7b00a
Logan <1009728013@qq.com>
2024-07-31 16:33:10 +0800

    feat: resource demo

---
+++ src/pages/Resource.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Items from '../components/Items.vue'
+import { ref, onMounted } from 'vue'
+import { getResources } from '../apis/api.js'
+
+onMounted(() => {
+  getResources().then(res => {
+    data.value = res.data.data.rows
+  })
+})
+
+const data = ref([])
+
+</script>
+
+<template>
+  <div class="max-w-150 m-0 m-auto">
+    <Items path="resource" :data="data" />
+  </div>
+</template>
+
+<style scoped></style>
