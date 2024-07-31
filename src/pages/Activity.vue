99cf99217ba3c94b56cecfe65b0236889395ca94
Logan <1009728013@qq.com>
2024-07-31 15:38:30 +0800

    feat: update activity

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -1,5 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { getActivities } from '../apis/api'
+
+onMounted(() => {
+  getActivities().then(res => {
+    data.value = res.data.data.rows
+  })
+})
+
+const router = useRouter()
+const data = ref([])
 
 const carouselData = ref([
   { coverPicture: '/act-image-1.jpg' },
@@ -23,8 +34,8 @@ const carouselData = ref([
     <div class="h-8"></div>
 
     <div class="flex flex-wrap justify-center">
-      <div class="w-55 h-40 mx-2 mb-4" v-for="item in 12">
-        <img class="w-full h-full shadow-2xl rounded-xl" src="/poster.jpg">
+      <div class="w-55 h-40 mx-2 mb-4" v-for="item in data">
+        <img class="w-full h-full shadow-2xl rounded-xl hover:cursor-pointer" :src="item.coverPicture" @click="router.push(`/activity/${item.id}`)">
       </div>
     </div>
 
