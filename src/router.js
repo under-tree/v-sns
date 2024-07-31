839ec614bb3d446f63c36dcb2f1f747971f10aff
Logan <1009728013@qq.com>
2024-07-31 16:56:13 +0800

    feat: update router

--- src/router.js
+++ src/router.js
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import PostDetail from './pages/PostDetail.vue'
 import Activity from './pages/Activity.vue'
-import ActivityDetail from './pages/ActivityDetail.vue'
+import ActivityDetail from './pages/PostDetail.vue'
 import Job from './pages/Job.vue'
-// import JobDetail from './pages/JobDetail.vue'
-// import Resource from './pages/Resource.vue'
-// import ResourceDetail from './pages/ResourceDetail.vue'
+import JobDetail from './pages/PostDetail.vue'
+import Resource from './pages/Resource.vue'
+import ResourceDetail from './pages/PostDetail.vue'
+import NoticeDetail from './pages/PostDetail.vue'
 import Panel from './pages/Panel.vue'
 import User from './pages/User.vue'
 import Editor from './pages/Editor.vue'
@@ -17,9 +18,10 @@ const routes = [
   { 'path': '/activity', 'component': Activity },
   { 'path': '/activity/:id', 'component': ActivityDetail },
   { 'path': '/job', 'component': Job },
-  // { 'path': '/job/:id', 'component': JobDetail },
-  // { 'path': '/resource', 'component': Resource },
-  // { 'path': '/resource/:id', 'component': ResourceDetail },
+  { 'path': '/job/:id', 'component': JobDetail },
+  { 'path': '/resource', 'component': Resource },
+  { 'path': '/resource/:id', 'component': ResourceDetail },
+  { 'path': '/notice/:id', 'component': NoticeDetail },
   { 'path': '/user', 'component': Panel },
   { 'path': '/users/:id', 'component': User },
   { 'path': '/editor', 'component': Editor },
